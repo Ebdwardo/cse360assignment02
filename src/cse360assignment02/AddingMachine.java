95725fd3b11e7b762360a1822288ec1348d8e522
Eduardo Teodosio <eteodosi@asu.edu>
2020-10-03 19:43:05 -0700

    started the project

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    // total of the adding machine
+    private int total;
+
+    // constructor
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    // returns the total
+    public int getTotal () {
+        return 0;
+    }
+
+    // add value to the total and the memory
+    public void add (int value) {
+    }
+
+    // subtracts value from the total and the memory
+    public void subtract (int value) {
+    }
+
+    // shows all the operations that have been done to the total
+    public String toString () {
+        return "";
+    }
+
+    // resets the total and the result string
+    public void clear() {
+    }
+}
