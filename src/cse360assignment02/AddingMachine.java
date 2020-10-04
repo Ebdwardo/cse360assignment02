207907ae8df8fc0c5a30abc70075f48beacb55a9
Eduardo Teodosio <eteodosi@asu.edu>
2020-10-03 20:24:50 -0700

    added Javadoc style comments

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,31 +1,56 @@
 package cse360assignment02;
 
+/**
+ * This class is a basic calculator. It performs add and subtract functions and displays the order in which they happen
+ */
 public class AddingMachine {
     private int total;
     private String result = "0 ";
 
+    /**
+     * Constructor for the AddingMachine class
+     */
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
     }
 
+    /**
+     * Returns the current total
+     * @return current total for the AddingMachine
+     */
     public int getTotal () {
         return total;
     }
 
+    /**
+     * Adds value to the total
+     * @param value value that gets added to the total
+     */
     public void add (int value) {
         total = total + value;
         result+= "+ " + value + " ";
     }
 
+    /**
+     * Subtracts the value from the total
+     * @param value value that gets subtracted from the total
+     */
     public void subtract (int value) {
         total = total - value;
         result+= "- " + value + " ";
     }
 
+    /**
+     * Returns a string showing all the operations that have happened to the total
+     * @return returns result, the string that shows all the operations that have happened
+     */
     public String toString () {
         return result;
     }
 
+    /**
+     * Resets the total and clears the string result
+     */
     public void clear() {
         total = 0;
         result = "0 ";
